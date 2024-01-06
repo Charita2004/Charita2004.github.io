c19f7223a02b2e9f87d30feb017a689a25f65b45
Charita2004 <148785328+Charita2004@users.noreply.github.com>
2024-01-07 04:35:34 +0700

    Delete RinaBen directory

--- RinaBen/offer.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.gallery img').forEach(img => {
-    img.addEventListener('click', () => {
-        // Mã thực thi khi người dùng click vào ảnh
-        console.log('Image clicked:', img.alt);
-    });
-});
